0dd98eaa3687fcd6495a45353fc0c377f2f658c3
Saket Verma <saket3324verma@gmail.com>
2021-04-25 00:13:36 +0530

    failed at animation

--- src/app/arranged-cards/arranged-cards.component.ts
+++ src/app/arranged-cards/arranged-cards.component.ts
@@ -7,10 +7,76 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ArrangedCardsComponent implements OnInit {
 
+    @Input() cards: any;
+    @Input() skip: number;
+    display: any[] = [];
+
+    check: number = 0;
+    innerCheck = 0;
+
     constructor() { }
 
     ngOnInit(): void {
+        this.arrangeCards(this);
+    }
 
+    arrangeCards(that) {
+        console.table("Cards", this.cards);
+        console.table("Display", this.display);
+
+        setTimeout(() => {
+            for (let i: number = 0; i < this.cards.length; i++) {
+
+                //Remove first card
+                if (this.check == 1) {
+                    this.display.push(this.cards[0].value);
+                    for (let j: number = 0; j < this.cards.length; j++) {
+                        var temp;
+                        if (j == 0) {
+                            temp = this.cards[0];
+                        }
+                        if (j + 1 == this.cards.length) {
+                            this.cards[j] = temp;
+                        } else {
+                            this.cards[j] = this.cards[j + 1];
+                        }
+                    }
+                    console.log("Card Popped : ", this.cards[this.cards.length - 1])
+                    this.cards.pop();
+                    this.check--;
+                    this.innerCheck = 0;
+                    setTimeout(() => {
+                        this.arrangeCards(that);
+                    }, 1000);
+                }
+
+
+                //Move skip card ahead
+                if (this.check == 0) {
+                    for (let j: number = 0; j < this.cards.length; j++) {
+                        var temp;
+                        if (j == 0) {
+                            temp = this.cards[0];
+                        }
+                        if (j + 1 == this.cards.length) {
+                            this.cards[j] = temp;
+                        } else {
+                            this.cards[j] = this.cards[j + 1];
+                        }
+                    }
+                    this.innerCheck = this.innerCheck + 1;
+
+                    if (this.innerCheck == this.skip) {
+                        this.check = 1;
+                    }
+                }
+
+
+            }
+            if (this.cards.length > 0) {
+                this.arrangeCards(that);
+            }
+        }, 1000);
     }
 
 }
